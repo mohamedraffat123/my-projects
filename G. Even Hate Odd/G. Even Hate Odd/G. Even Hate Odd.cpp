9fce0864076c5e4c4463d56fd5396a2f4cd79d86
mohmad raafat <116029713+mohamedraffat123@users.noreply.github.com>
2024-04-07 18:37:19 +0200

    Add files via upload

---
+++ G. Even Hate Odd/G. Even Hate Odd/G. Even Hate Odd.cpp
@@ -0,0 +1,46 @@
+// G. Even Hate Odd.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n,*z,f;
+    cin >> n;
+    z = new long long[n];
+    while (n--) {
+        cin >> f;
+        int even=0, odd=0;
+        for (int i = 0; i < f; i++) {
+            int value;
+            cin >> value;
+            if (value % 2 == 0) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+        }
+        if (even == odd) {
+            cout << 0 << endl;
+        }
+        else if((even+odd)%2==1) {
+            cout << -1 << endl;
+        }
+        else {
+            cout << abs(even - odd) / 2 << endl;
+        }
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ G. Palindrome Array/G. Palindrome Array/G. Palindrome Array.cpp
@@ -0,0 +1,41 @@
+// G. Palindrome Array.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long n, * z, left = 0,r;
+    cin >> n;
+    z = new long long[n];
+    long long right = n - 1;
+    long x;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> z[i];
+    }
+    while (z[right] == z[left] && left<right)
+    {
+            right--;
+            left++;
+    }
+    if (left<right) {
+        cout << "NO";
+    }
+    else {
+        cout << "YES";
+    }
+
+	return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
