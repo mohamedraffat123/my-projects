3f7f0c57ec126a9646a4d5a0c7f3b0b0421f155d
mohmad raafat <116029713+mohamedraffat123@users.noreply.github.com>
2024-04-07 18:36:36 +0200

    Add files via upload

---
+++ G. Construct the Sum/G. Construct the Sum/G. Construct the Sum.cpp
@@ -0,0 +1,49 @@
+// G. Construct the Sum.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+int arr[1000005];
+using namespace std;
+int main()
+{
+    long n, cou=0;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        long long x, y;
+        cin >> x >> y;
+        if (x * (x + 1) / 2 < y) {
+            cout << -1;
+        }
+        else {
+            long long sum = 0;
+            for (int j = x; j >= 1; j--) {
+                if (sum + j <= y) {
+                    sum += j;
+                    arr[cou] = j;
+                    cou++;
+                }
+                if (sum == y) {
+                    break;
+                }
+            }
+        }
+            for (int z = 0; z < cou; z++) {
+                cout << arr[z] << " ";
+            }
+            cout << endl;
+            cou = 0;
+    }
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
